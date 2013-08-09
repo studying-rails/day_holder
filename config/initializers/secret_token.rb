3576646ecf63343db6ce5d890f330a640a4ff055
太田 大地 <daichi@ground-air.tok.access-company.com>
2013-08-09 09:08:45 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DayHolder::Application.config.secret_token = 'ac3f10bbe3e0be8ac0f887376004c2a78c6a058489d3ec48b17bb21b567d4a10fb11c928a62548be9a852f2414c3497e31abf4c25b24a37f75026561fffd3a3e'
